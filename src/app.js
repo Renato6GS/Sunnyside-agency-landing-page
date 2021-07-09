c78928d0f2ef3c697371a0bcebf48eb2a59fbc61
Renatogranados <58044053+Renatogranados@users.noreply.github.com>
2021-07-08 21:45:13 -0300

    feat: webpack was integrated and several files were redistributed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+function importAll(r) {
+   return r.keys().map(r);
+}
+importAll(require.context('./static/img', false, /\.(png|jpe?g|svg)$/));
+importAll(require.context('./static/img/desktop', false, /\.(png|jpe?g|svg)$/));
+
+import './vendors/normalize/normalize.css';
+import './styles/main.scss';
+
 const activeBars = document.getElementById('active-bars'),
    menu = document.getElementById('container-nav'),
    titleHeader = document.getElementById('title-header'),
